ddc6bf0c6f9fd968550223f1879124c8e4d84dfe
Chloe Jiang <yueyue426@berkeley.edu>
2025-09-05 15:46:56 -0700

    Update methods log_progress and extract definition

--- banks_project.py
+++ banks_project.py
@@ -1,5 +1,7 @@
 # Code for ETL operations on Country-GDP data
 # Import necessary libraries
+from operator import index
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -8,18 +10,43 @@ import numpy as np
 from datetime import datetime
 
 def log_progress(message):
-    '''
+    """
     This function logs the mentioned message of a given stage of
     the code execution to a log file.
     Function returns nothing
-    '''
+    """
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour:Minute:Second
+    now = datetime.now() # get the current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("./code_log.txt", "a") as f:
+        f.write(timestamp + " : " + message + "\n")
 
 def extract(url, table_attribs):
-    '''
+    """
     This function aims to extract the required information from
     the provided url and save it to a data frame.
     The function returns the data frame
-    '''
+    """
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    tables = soup.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    df = pd.DataFrame(columns=table_attribs)
+
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) != 0:
+            links = cols[1].find_all('a')
+            bank_name = links[1].contents[0]
+            market_cap_raw = cols[2].contents[0].replace('\n', '')
+            market_cap = float(market_cap_raw) # cast data type as float
+
+            data_dict = {
+                "Name": bank_name,
+                "MC_USD_Billion": market_cap
+            }
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
 
     return df
 
